2b7651f645135aaf06b3ecedbe5e8da10f021073
btzr-io <btzr.io@gmail.com>
2021-02-05 00:58:06 -0600

    use env variables for elasticsearch auth

--- src/elastic.js
+++ src/elastic.js
@@ -1,4 +1,9 @@
 import { Client } from "@elastic/elasticsearch";
-const client = new Client({ node: "http://localhost:9200" });
+import { config } from 'dotenv';
+
+// Load config from env
+config();
+
+const client = new Client({ node: process.env.ELASTIC_NODE, auth: { username: process.env.ELASTIC_USER, password: process.env.ELASTIC_PASSWORD }});
 
 export default client;
